e95903b3bf2abb05bbb60f01c698aecf95e70636
sunhairui <895892673@qq.com>
2020-02-07 23:30:32 +0800

    修改了一个控制器的代码

--- src/main/java/com/sunhairui/demo/MyController.java
+++ src/main/java/com/sunhairui/demo/MyController.java
@@ -1,16 +1,6 @@
 package com.sunhairui.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 
 @Controller

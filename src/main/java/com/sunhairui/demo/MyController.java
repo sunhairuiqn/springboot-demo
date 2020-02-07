98700b9287558f354e27520a4844f2d3f90805a1
sunhairui <895892673@qq.com>
2020-02-07 23:33:10 +0800

    fuck the world

--- src/main/java/com/sunhairui/demo/MyController.java
+++ src/main/java/com/sunhairui/demo/MyController.java
@@ -1,11 +1,15 @@
 package com.sunhairui.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class MyController {
 
-
+    @RequestMapping("/fuck")
+    public String fuck(){
+        return " fuck the world";
+    }
 
 }
